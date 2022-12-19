34f7b36d287977b7c068e486f8ce53f26b027f76
Joe-Collins-1986 <collins.jw@btinternet.com>
2022-12-19 17:36:22 +0000

    set x_frame_options

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,6 +31,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 ALLOWED_HOSTS = ['django-blog-codestar-p4-test.herokuapp.com', 'localhost']
 
 
